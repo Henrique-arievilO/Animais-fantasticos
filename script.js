09a0e8427e4249b1fa3456abe97eda3d87d07aaa
Henrique Oliveira <wandholiveira1@gmail.com>
2024-12-28 20:16:12 -0300

    :black_bird: A function for Accordion List was created in the frequently asked questions section. The functions were encapsulated in larger functions in order to bring greater organization and autonomy to the code.

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
+const activeClass = 'active';
+
 //Função pra linkar as imagens nas respectivas seções;
+
 function tabNavigation () {
-  console.log('teste');
+  const tabMenu = document.querySelectorAll('.js-tabMenu li');
+  const tabContent = document.querySelectorAll('.js-tabContent section');
+  
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(activeClass);
+  
+    function activeTab (index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(activeClass);
+      })
+      tabContent[index].classList.add(activeClass);
+    };
+    
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      })
+    })
+  };
 }
 tabNavigation();
 
-const tabMenu = document.querySelectorAll('.js-tabMenu li');
-const tabContent = document.querySelectorAll('.js-tabContent section');
-const activeClass = 'active';
-
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add(activeClass);
-
-  function activeTab (index) {
-    tabContent.forEach((section) => {
-      section.classList.remove(activeClass);
-    })
-    tabContent[index].classList.add(activeClass);
-  };
+function navFaqList () {
+  const accordionList = document.querySelectorAll('.js-accordionList dt');
+  if(accordionList.length) {
   
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.toggle(activeClass);
+    
+    function activeAccordion () {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
     })
-  })
-};
+  }
+}
+navFaqList();
