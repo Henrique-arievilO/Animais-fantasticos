66c496be1b5098c2d1cc15d705a2b0a988e580fe
Henrique Oliveira <wandholiveira1@gmail.com>
2024-12-29 21:20:12 -0300

    :black_bird: Created a function to smoothly scroll the navigation menu to the respective section.

--- script.js
+++ script.js
@@ -43,3 +43,26 @@ function navFaqList () {
   }
 }
 navFaqList();
+
+function smoothScroll () {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+  
+  function scrollToSection (event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+  
+    section.scrollIntoView(
+      {
+        behavior: 'smooth',
+        block: 'start',
+      }
+    );
+  }
+  internalLinks.forEach((item) => {
+    item.addEventListener('click', scrollToSection);
+  });
+}
+smoothScroll();
+
+
